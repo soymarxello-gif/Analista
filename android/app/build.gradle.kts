495e91bec873fea1d4f3a02fa0b15dcc0ab8a062
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 15:38:32 -0400

    Bump Android version to 1.28.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.27.0"
+        versionCode = 31
+        versionName = "1.28.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
