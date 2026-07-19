97c3ded5f928db0293b98f28ef2bee862687e258
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 15:31:33 -0400

    Bump Android version to 1.27.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.26.0"
+        versionCode = 30
+        versionName = "1.27.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
