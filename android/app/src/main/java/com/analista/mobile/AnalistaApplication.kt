42b369cabaeb9488b4fac0d1d2c4142879e21eb4
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 10:24:35 -0400

    Wire Alpaca gateway into application

--- android/app/src/main/java/com/analista/mobile/AnalistaApplication.kt
+++ android/app/src/main/java/com/analista/mobile/AnalistaApplication.kt
@@ -2,11 +2,14 @@ package com.analista.mobile
 
 import android.app.Application
 import androidx.room.Room
+import com.analista.mobile.data.AlpacaCredentialsStore
+import com.analista.mobile.data.AlpacaMarketDataClient
 import com.analista.mobile.data.AnalistaDatabase
 import com.analista.mobile.data.MIGRATION_1_2
 import com.analista.mobile.data.MIGRATION_2_3
 import com.analista.mobile.data.MIGRATION_3_4
 import com.analista.mobile.data.MIGRATION_4_5
+import com.analista.mobile.data.MarketDataGateway
 import com.analista.mobile.data.ScanRepository
 import com.analista.mobile.data.YahooFinanceClient
 
@@ -16,5 +19,10 @@ class AnalistaApplication : Application() {
             .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
             .build()
     }
-    val repository by lazy { ScanRepository(database.dao(), YahooFinanceClient(this)) }
+    private val yahoo by lazy { YahooFinanceClient(this) }
+    val credentialsStore by lazy { AlpacaCredentialsStore(this) }
+    val marketDataGateway by lazy {
+        MarketDataGateway(yahoo, AlpacaMarketDataClient(), credentialsStore)
+    }
+    val repository by lazy { ScanRepository(database.dao(), yahoo, marketDataGateway) }
 }
