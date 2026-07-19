d374e24db2690f68e78f01614db0ad371e92d2a0
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 16:05:40 -0400

    Actualizar Android a 1.29.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "1.28.0"
+        versionCode = 32
+        versionName = "1.29.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
