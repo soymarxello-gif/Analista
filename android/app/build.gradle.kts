77a490194a0a09d73a88a96ff87cb611aa554c85
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 09:50:09 -0400

    Habilitar pruebas JVM del parser JSON

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.json:json:20240303")
 }
