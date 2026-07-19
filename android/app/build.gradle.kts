c3b08da86dc25162fa554811d1d197ece5e3ced7
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 14:23:02 -0400

    Android v2: subir versión a 1.20.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.19.0"
+        versionCode = 23
+        versionName = "1.20.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
