ff7d907666ba3c90d6496e01fa099bb7682069fd
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 13:25:17 -0400

    Actualizar versión Android a 1.12.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.11.0"
+        versionCode = 15
+        versionName = "1.12.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
