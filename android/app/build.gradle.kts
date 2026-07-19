637a0ec8d13405393fc06502071e5c947bfcf91e
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 14:05:45 -0400

    Android: versión 1.17.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.16.0"
+        versionCode = 20
+        versionName = "1.17.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
