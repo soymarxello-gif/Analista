54ae78c25cc342dd463a09b907f28cff6ae017ff
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 09:48:22 -0400

    Elevar esquema Room para contexto de ejecución

--- android/app/src/main/java/com/analista/mobile/data/AnalistaDatabase.kt
+++ android/app/src/main/java/com/analista/mobile/data/AnalistaDatabase.kt
@@ -11,7 +11,7 @@ import androidx.room.RoomDatabase
         MarketSnapshotEntity::class,
         CandidateEnrichmentEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = true
 )
 abstract class AnalistaDatabase : RoomDatabase() {
