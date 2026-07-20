70a3a7a526f29277a3003d0e85fbd1114fb81645
soymarxello-gif <soymarxello@gmail.com>
2026-07-20 10:07:27 -0400

    Bump Android to 1.49.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "1.48.0"
+        versionCode = 52
+        versionName = "1.49.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
