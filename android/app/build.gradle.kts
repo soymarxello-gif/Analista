1c468dee093a2ab3dfb862d78ae955da3ec01d65
soymarxello-gif <soymarxello@gmail.com>
2026-07-20 14:14:29 -0400

    Bump Android release candidate to 1.66.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "1.66.0"
+        versionCode = 70
+        versionName = "1.66.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
