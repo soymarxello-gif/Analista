ba9f573d7f67a1ff875d32917f209a4f12b7ba03
soymarxello-gif <soymarxello@gmail.com>
2026-07-20 11:06:48 -0400

    Bump Android to 1.54.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 56
-        versionName = "1.53.0"
+        versionCode = 57
+        versionName = "1.54.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
