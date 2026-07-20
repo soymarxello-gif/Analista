11acbd1a12a79725875e6bfc590c737162246a44
soymarxello-gif <soymarxello@gmail.com>
2026-07-19 23:22:50 -0400

    Test Yahoo provider quote timestamps

--- android/app/src/test/java/com/analista/mobile/data/YahooMarketQuoteTest.kt
+++ android/app/src/test/java/com/analista/mobile/data/YahooMarketQuoteTest.kt
@@ -10,7 +10,7 @@ class YahooMarketQuoteTest {
     private val client = YahooFinanceClient(mock<Context>())
 
     @Test
-    fun parsesPremarketExecutionFields() {
+    fun parsesPremarketExecutionFieldsAndProviderTimestamp() {
         val json = """{
           "quoteResponse": {"result": [{
             "symbol": "TEST",
@@ -18,18 +18,36 @@ class YahooMarketQuoteTest {
             "ask": 101.2,
             "regularMarketPrice": 100.0,
             "preMarketPrice": 101.1,
+            "preMarketTime": 1700000000,
+            "regularMarketTime": 1699990000,
             "marketCap": 5000000000,
             "quoteType": "EQUITY",
             "marketState": "PRE"
           }], "error": null}
         }"""
-        val quote = client.parseMarketQuote(json, "TEST")
+        val retrievedAt = 1_700_000_100_000L
+        val quote = client.parseMarketQuote(json, "TEST", retrievedAt)
         assertEquals(101.0, quote.bid!!, 0.0001)
         assertEquals(101.2, quote.ask!!, 0.0001)
         assertEquals(101.1, quote.preMarketPrice!!, 0.0001)
         assertEquals(5_000_000_000L, quote.marketCap)
         assertEquals("EQUITY", quote.quoteType)
         assertEquals("PRE", quote.marketState)
+        assertEquals(1_700_000_000_000L, quote.providerTimestampUtc)
+        assertEquals(1_700_000_000_000L, quote.capturedAtUtc)
+        assertEquals(retrievedAt, quote.retrievedAtUtc)
+        assertEquals("YAHOO", quote.provider)
+    }
+
+    @Test
+    fun missingProviderTimestampRemainsUnknownInsteadOfUsingRetrievalTime() {
+        val json = """{"quoteResponse":{"result":[{
+          "symbol":"TEST","bid":100,"ask":100.1,"regularMarketPrice":100,
+          "marketCap":5000000000,"quoteType":"EQUITY"
+        }],"error":null}}"""
+        val quote = client.parseMarketQuote(json, "TEST", 1_700_000_100_000L)
+        assertNull(quote.providerTimestampUtc)
+        assertEquals(1_700_000_100_000L, quote.capturedAtUtc)
     }
 
     @Test
