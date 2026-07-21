84881638b38ad5531f56ef5987c84b6702de2362
soymarxello-gif <soymarxello@gmail.com>
2026-07-21 10:03:16 -0400

    Bump Android to 1.73.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.analista.mobile.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 76
-        versionName = "1.72.0"
+        versionCode = 77
+        versionName = "1.73.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
