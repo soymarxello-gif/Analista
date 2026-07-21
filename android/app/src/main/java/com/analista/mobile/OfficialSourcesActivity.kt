9e29a6ccf03789b76473ce1b78cb7f5fc132dce8
soymarxello-gif <soymarxello@gmail.com>
2026-07-21 10:33:02 -0400

    Opt in official source top app bar

--- android/app/src/main/java/com/analista/mobile/OfficialSourcesActivity.kt
+++ android/app/src/main/java/com/analista/mobile/OfficialSourcesActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -53,6 +54,7 @@ class OfficialSourcesActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun OfficialSourcesScreen(viewModel: MainViewModel, close: () -> Unit) {
     val state by viewModel.officialSources.collectAsState()
